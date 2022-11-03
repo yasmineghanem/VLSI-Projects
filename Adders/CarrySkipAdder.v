723cae6d4f3923d338ab744fcbdc865e41280532
Yasmine Ghanem <74925701+yasmineghanem@users.noreply.github.com>
2022-11-03 10:32:08 +0200

    Fixed carry skip adder

--- Adders/CarrySkipAdder.v
+++ Adders/CarrySkipAdder.v
@@ -9,22 +9,15 @@ module SkipLogic #(parameter N = 4) (A,B,Cin,Cout,Next_Cin);
   wire [3:0] Xors;
   wire [1:0] Ands; 
     
-  //assign Xors[0] = A[0] ^ B[0];
-  //assign Xors[1] = A[1] ^ B[1];
-  //assign Xors[2] = A[2] ^ B[2];
-  //assign Xors[3] = A[3] ^ B[3];
-  xor (Xors[0], A[0], B[0]);
-  xor (Xors[0], A[1], B[1]);
-  xor (Xors[0], A[2], B[2]);
-  xor (Xors[0], A[3], B[3]);
+  assign Xors[0] = A[0] ^ B[0];
+  assign Xors[1] = A[1] ^ B[1];
+  assign Xors[2] = A[2] ^ B[2];
+  assign Xors[3] = A[3] ^ B[3];
   
-  //assign Ands[0] = Xors[0] & Xors[1] & Xors[2] & Xors[3];
-  and(Ands[0], Xors[0],  Xors[1],  Xors[2],  Xors[3]);
-  and(Ands[1], Ands[0], Cin);
-  //assign Ands[1] = Ands[0] & Cin;
+  assign Ands[0] = Xors[0] & Xors[1] & Xors[2] & Xors[3];
+  assign Ands[1] = Ands[0] & Cin;
   
   assign Next_Cin = Ands[1] | Cout;
-  //or(Next_Cin, Ands[1], Cout);
 
 endmodule
 
