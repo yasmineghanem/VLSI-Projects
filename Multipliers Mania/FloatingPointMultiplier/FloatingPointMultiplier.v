a55912794ce39f09874c733801aa0006a1132178
Yasmine Ghanem <74925701+yasmineghanem@users.noreply.github.com>
2022-12-13 23:15:51 +0200

    floating point testbench done

--- Multipliers Mania/FloatingPointMultiplier/FloatingPointMultiplier.v
+++ Multipliers Mania/FloatingPointMultiplier/FloatingPointMultiplier.v
@@ -1,22 +1,23 @@
 `include "../../Adders/PlusAdder.v"
 
-module FLoatingPointMultiplier (a, b, product);
+module FLoatingPointMultiplier (a, b, product, infinity, NAN, overflow);
     //input and output ports
     input [31:0] a, b;
     output [31:0] product;
+    output infinity, NAN, overflow;
 
     //intermediate wires
     wire finalSign;
     wire [22:0] mantissaA, mantissaB;
     wire [7:0] expA, expB;
-    wire earlyZero;
+    wire zero;
 
     wire [23:0] significandA; 
     wire [23:0] significandB;
 
-    //Check for zero cases
+    //Check for zero case
     //if A is zero -> mantissa and exp are both zero
-    assign earlyZero = 
+    assign zero = 
     ((mantissaA == {23{1'b0}} && expA == {8{1'b0}}) || (mantissaB == {23{1'b0}} && expB == {8{1'b0}})) ? 
         1'b1 : 1'b0;
 
@@ -34,32 +35,41 @@ module FLoatingPointMultiplier (a, b, product);
     wire [47:0] significandProduct;
 
     //Adder wires
-    wire cout, overflow;
+    wire cout;
 
+    //Add exponents 
     PlusAdder #(8) Adder(expA,expB,expSum,cout,overflow);
 
+    //sbutract bias from added exponents
     wire [7:0] expBias;
     assign expBias = {cout,expSum} - 8'd127;
 
-    // assign expSum = (expA + expB) - 8'b011111111; //subtract 127 (bias) from exp
-
-    // multiply both significants the product is 48 bits
+    // multiply both significands the product is 48 bits
+    //round number take 23 MSBs
     assign significandProduct = significandA * significandB;
-    wire [24:0] m3lsh, shiftedProduct;
+    wire [24:0] shiftedProduct;
     wire [22:0] normMantissa;
     wire normalize;
 
-    assign m3lsh = significandProduct[47:23];
     assign normalize = significandProduct[47];
 
-    assign shiftedProduct = m3lsh>>1;
+    assign shiftedProduct = significandProduct[47:23]>>1;
 
     assign normMantissa =
-        (normalize == 1'b1) ? shiftedProduct[22:0] : m3lsh[22:0];
+        (normalize == 1'b1) ? shiftedProduct[22:0] : significandProduct[45:23];
     
     wire [7:0] normExp;
     assign normExp = (normalize == 1'b1) ? expBias+1 : expBias;
 
-    assign product = {finalSign, normExp, normMantissa};
+    //check for infinty case 
+    assign infinity =
+        ((normExp == {8{1'b1}}) && (normMantissa == {23{1'b0}})) ? 1'b1 : 1'b0;
+
+    //check for NAN case 
+    assign NAN =
+        ((normExp == {8{1'b1}}) && (normMantissa != {23{1'b0}})) ? 1'b1 : 1'b0;
+    
+
+    assign product = (zero == 1'b1) ? {32{1'b0}} : {finalSign, normExp, normMantissa};
     
 endmodule
--- Multipliers Mania/FloatingPointMultiplier/FloatingPointMultiplierTB.v
+++ Multipliers Mania/FloatingPointMultiplier/FloatingPointMultiplierTB.v
@@ -1,48 +1,105 @@
 module FLoatingPointMultiplierTB();
-    reg [31:0] b,a;
+    reg [31:0] a,b;
     wire [31:0] product;
+    wire infinity, NAN, overflow;
 
-    FLoatingPointMultiplier FPMultiplierModule(a,b,product);
+    FLoatingPointMultiplier FPMultiplierModule(.a(a),.b(b),.product(product),.infinity(infinity),.NAN(NAN),.overflow(overflow));
+
         initial begin
+
+            $monitor(a,b,product,overflow);
+
+                    //Initial values
+            a={32{1'b0}}; b={32{1'b0}};
+            #100
 
             //Testcase(1): positive x positive
             a = 32'b01000010101010100100000000000000; //85.125
             b = 32'b01000010001101001000000000000000; //45.125
+            #20
+            if(product == 32'b01000101011100000001010001000000) begin
+                $display("TESTCASE#1: SUCCESS");
+            end 
+            else begin
+                $display("TESTCASE#1 failed with input a=%b and a=%b and output product=%b and overflow status =%b",a, b, product, overflow);
+            end
 
             #100
             //Testcase(2): negative x negative
             a = 32'b11000000101100000000000000000000; //-5.5
             b = 32'b11000001001010000000000000000000; //-10.5
+            #20
+            if(product == 32'b01000010011001110000000000000000) begin
+                $display("TESTCASE#2: SUCCESS");
+            end else begin
+                $display("TESTCASE#2 failed with input a=%b and a=%b and output product=%b and overflow status =%b", a, b, product, overflow);
+            end
 
             #100
             //Testcase(3): positive x negative
             a = 32'b01000000110010000000000000000000; //6.25
             b = 32'b11000000010110011001100110011010; //-3.4
+            #20             
+            if(product == 32'b11000001101010100000000000000000) begin
+                $display("TESTCASE#3: SUCCESS");
+            end else begin
+                $display("TESTCASE#3 failed with input a=%b and a=%b and output product=%b and overflow status =%b", a, b, product, overflow);
+            end
 
             #100
             //Testcase(4): negative x positive
             a = 32'b11000000101100000000000000000000; //-5.5
             b = 32'b01000000110010000000000000000000; //6.25
+            #20
+            if(product == 32'b11000010000010011000000000000000) begin
+                $display("TESTCASE#4: SUCCESS");
+            end else begin
+                $display("TESTCASE#4 failed with input a=%b and a=%b and output product=%b and overflow status =%b", a, b, product, overflow);
+            end
 
             #100
             //Testcase(5): multiplication by 0
             a = 32'b01000010101010100100000000000000; //85.125
             b = {32{1'b0}}; //0
+            #20
+            if(product == {32{1'b0}}) begin
+                $display("TESTCASE#5: SUCCESS");
+            end else begin
+                $display("TESTCASE#5 failed with input a=%b and a=%b and output product=%b and overflow status =%b", a, b, product, overflow);
+            end
 
             #100
             //Testcase(6): multiplication by 1
             a = 32'b01000010001101001000000000000000; //45.125
             b = 32'b00111111100000000000000000000000; //1
+            #20
+            if(product == 32'b01000010001101001000000000000000) begin
+                $display("TESTCASE#6: SUCCESS");
+            end else begin
+                $display("TESTCASE#6 failed with input a=%b and a=%b and output product=%b and overflow status =%b", a, b, product, overflow);
+            end
 
            #100
             //Testcase(7): additional testcase: float x int
             a = 32'b01000000110100000000000000000000; //6.5
             b = 32'b01000000010000000000000000000000; //3
+            #20
+            if(product == 32'b01000001100111000000000000000000) begin
+                $display("TESTCASE#7: SUCCESS");
+            end else begin
+                $display("TESTCASE#7 failed with input a=%b and a=%b and output product=%b and overflow status =%b", a, b, product, overflow);
+            end
 
             #100
             //Testcase(8): additional testcase: 
-            a = 32'b01000010101010100100000000000000; //85.125
-            b = 32'b01000010001101001000000000000000; //45.125
+            a = 32'b00111111100000000000000000000000; //1
+            b = 32'b10111111100000000000000000000000; //-1
+            #20
+            if(product == 32'b10111111100000000000000000000000) begin
+                $display("TESTCASE#8: SUCCESS");
+            end else begin
+                $display("TESTCASE#8 failed with input a=%b and a=%b and output product=%b and overflow status =%b", a, b, product, overflow);
+            end
         end
     
 endmodule
