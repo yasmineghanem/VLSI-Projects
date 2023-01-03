a7c7a0f9731424655913e3bac9c4f5e949739e00
YasminElgendi <yasminelgendi@gmail.com>
2023-01-04 01:26:57 +0200

    ops

--- Multipliers Mania/FloatingPointMultiplier/FPIntegrated.v
+++ Multipliers Mania/FloatingPointMultiplier/FPIntegrated.v
@@ -1,24 +1,21 @@
 `include "FloatingPointMultiplier.v"
 `include "../Register.v"
 
-module FPIntegrated (clk, a, b, writeEnableA, writeEnableB, writeEnableOut, readEnableA, readEnableB, readEnableOut, resetA, resetB, resetOut, product, overflow, infinity, NAN, accessErrorA, accessErrorB, accessErrorOut);
+module FPIntegrated (clk, a, b, enableA, enableB, enableOut, resetA, resetB, resetOut, product, overflow, infinity, NAN);
     input clk;
-    input writeEnableA, writeEnableB, writeEnableOut;
-    input readEnableA, readEnableB, readEnableOut;
+    input enableA, enableB, enableOut;
     input resetA, resetB, resetOut;
     input [31:0] a, b;
     output [31:0] product;
     output infinity;
     output overflow, NAN;
-    output accessErrorA, accessErrorB, accessErrorOut;
-
 
     wire [31:0] registerOutA, registerOutB;
-    Register #(32) InputRegisterA(.clk(clk), .dataIn(a), .dataOut(registerOutA), .readEnable(readEnableA), .writeEnable(writeEnableA), .reset(resetA), .accessError(accessErrorA));
-    Register #(32) InputRegisterB(.clk(clk), .dataIn(b), .dataOut(registerOutB), .readEnable(readEnableA), .writeEnable(writeEnableB), .reset(resetB), .accessError(accessErrorB));
+    InputRegister  InputRegisterA(.clk(clk), .dataIn(a), .dataOut(registerOutA), .enableA(enableA), , .reset(resetA));
+    InputRegister InputRegisterB(.clk(clk), .dataIn(b), .dataOut(registerOutB), enableB(enableB), .reset(resetB));
 
     wire [31:0] registerInProduct;
     FLoatingPointMultiplier FPMultiplier(.a(registerOutA), .b(registerOutB), .product(registerInProduct), .overflow(overflow), .infinity(infinity), .NAN(NAN));
 
-    Register #(32) OutputRegister(.clk(clk), .dataIn(registerInProduct), .dataOut(product), .readEnable(readEnableOut), .writeEnable(writeEnableOut), .reset(resetOut), .accessError(accessErrorOut));
+    OutputRegister  OutputRegister(.clk(clk), .dataIn(registerInProduct), .dataOut(product), .enableOut(enableOut), .reset(resetOut));
 endmodule
--- Multipliers Mania/TB.v
+++ Multipliers Mania/TB.v
@@ -1,6 +1,6 @@
-`include "BAMIntegrated.v"
+`include "/BoothAlgorithm/BAMIntegrated.v"
 
-module BAMIntegratedIntegratedTB ();
+module GenericTB ();
     reg clk;
     reg enableA, enableB, enableOut;
     reg resetA, resetB, resetOut;
@@ -26,11 +26,11 @@ module BAMIntegratedIntegratedTB ();
         enableA = 1'b0; enableB = 1'b0; enableOut = 1'b0; 
         resetA = 1'b0; resetB = 1'b0; resetOut = 1'b0;
 
-        //Testcase(1): positive x positive: a = 5 b = 6       
+        //Testcase(1):  
         #20
         // enable input registers to write a and b values
-        a = 32'd5; //5
-        b = 32'd6; //6      
+        a = 32'h87234; //5
+        b = 32'h348; //6      
         enableA = 1'b1; enableB = 1'b1;
 
         #20
@@ -39,7 +39,7 @@ module BAMIntegratedIntegratedTB ();
 
         //check results
         #30
-        if(product != (32'd30)) begin
+        if(product != (64'h1BB6BAA0)) begin
             $display("TESTCASE#1 FAILED with inputs a=%d and b=%d and output product=%d", a, b, product);
         end else begin
             $display("TESTCASE#1: SUCCESS");
@@ -49,10 +49,10 @@ module BAMIntegratedIntegratedTB ();
         enableOut = 1'b0;
 /*---------------------------------------------------------------------------------------------------------------------------------*/
    
-        //Testcase(2): negative x negative: a = -2111 b = -552233       
+        //Testcase(2): 
         // enable input registers to write a and b values
-        a = -32'd4; //-4
-        b = -32'd7; //-7        
+        a = 32'h50647236; //-4
+        b = 32'h50612336; //-7        
         enableA = 1'b1; enableB = 1'b1;
 
         #20
@@ -61,7 +61,7 @@ module BAMIntegratedIntegratedTB ();
 
         //check results
         #30
-        if(product != (32'd28)) 
+        if(product != (64'h193DE4CED7437964)) 
         begin
             $display("TESTCASE#2 FAILED with inputs a=%d and b=%d and output product=%d", a, b, product);
         end else begin
@@ -72,10 +72,10 @@ module BAMIntegratedIntegratedTB ();
         enableOut = 1'b0;
 /*---------------------------------------------------------------------------------------------------------------------------------*/
         
-        //Testcase(3): positive x negative a = 10 b = -4     
+        //Testcase(3):     
         // enable input registers to write a and b values
-        a = 32'd10; //10
-        b = -32'd4; //-4     
+        a = 32'h87234; //10
+        b = 32'hFFFFFEFD; //-4     
         enableA = 1'b1; enableB = 1'b1;
 
         #20
@@ -84,7 +84,7 @@ module BAMIntegratedIntegratedTB ();
 
         //check results
         #30
-        if(product != (-32'd40)) begin
+        if(product != (64'hFFFFFFFF_F7747564)) begin
             $display("TESTCASE#3 FAILED with inputs a=%d and b=%d and output product=%d", a, b, product);
         end else begin
             $display("TESTCASE#3: SUCCESS");
@@ -93,10 +93,10 @@ module BAMIntegratedIntegratedTB ();
         enableOut  = 1'b0;
 /*---------------------------------------------------------------------------------------------------------------------------------*/
         
-        //Testcase(4): negative x positive a = -2111 b = 125   
+        //Testcase(4): 
         // enable input registers to write a and b values
-        a = -32'd50; //-50
-        b = 32'd5; //5      
+        a = 32'h50647236; //-50
+        b = 32'hB887CAAF; //5      
         enableA = 1'b1; enableB = 1'b1;
 
         #20
@@ -105,7 +105,7 @@ module BAMIntegratedIntegratedTB ();
 
         //check results
         #30
-        if(product != (-32'd250)) begin
+        if(product != (64'hE98E647F4142AEEA)) begin
             $display("TESTCASE#4 FAILED with inputs a=%d and b=%d and output product=%d", a, b, product);
         end else begin
             $display("TESTCASE#4: SUCCESS");
@@ -114,10 +114,10 @@ module BAMIntegratedIntegratedTB ();
         enableOut  = 1'b0;
 /*---------------------------------------------------------------------------------------------------------------------------------*/
         
-        //Testcase(5): multiplication by 0   
+        //Testcase(5): 
         // enable input registers to write a and b values
-        a = 32'd1234; //1234
-        b = {32{1'b0}}; //0        
+        a = 32'hFFFFFEFD; //1234
+        b = 32'h87234; //0        
         enableA = 1'b1; enableB = 1'b1;
 
         #20
@@ -126,7 +126,7 @@ module BAMIntegratedIntegratedTB ();
 
         //check results
         #30
-        if(product != ({32{1'b0}})) begin
+        if(product != ({64'hFFFFFFFFF7747564})) begin
             $display("TESTCASE#5 FAILED with inputs a=%d and b=%d and output product=%d", a, b, product);
         end else begin
             $display("TESTCASE#5: SUCCESS");
@@ -135,10 +135,10 @@ module BAMIntegratedIntegratedTB ();
         enableOut  = 1'b0;
 /*---------------------------------------------------------------------------------------------------------------------------------*/
         
-        //Testcase(6): multiplication by 1  
+        //Testcase(6): 
         // enable input registers to write a and b values
-        a = 32'd99; //123456789
-        b = 32'd1; //1     
+        a = 32'hB887CAAF; //123456789
+        b = 32'h50647236; //1     
         enableA = 1'b1; enableB = 1'b1;
 
         #20
@@ -147,7 +147,7 @@ module BAMIntegratedIntegratedTB ();
 
         //check results
         #30
-        if(product != (32'd99)) begin
+        if(product != (64'h39F2D6B54142AEEA)) begin
             $display("TESTCASE#6 FAILED with inputs a=%d and b=%d and output product=%d", a, b, product);
         end else begin
             $display("TESTCASE#6: SUCCESS");
@@ -157,9 +157,10 @@ module BAMIntegratedIntegratedTB ();
         enableOut  = 1'b0;
 /*---------------------------------------------------------------------------------------------------------------------------------*/
         
-        //Testcase(7): additional testcase: 0 x 0
+        //Testcase(7): 
         // enable input registers to write a and b values
-        a = {32{1'b0}}; b = {32{1'b0}};         
+        a = {32'hFFFFFEFD};
+        b = {32'hFFFFFEFD};         
         enableA = 1'b1; enableB = 1'b1;
 
         #20
@@ -168,7 +169,7 @@ module BAMIntegratedIntegratedTB ();
 
         //check results
         #30
-        if(product != ({32{1'b0}})) begin
+        if(product != ({64'h10609})) begin
             $display("TESTCASE#7 FAILED with inputs a=%d and b=%d and output product=%d", a, b, product);
         end else begin
             $display("TESTCASE#7: SUCCESS");
@@ -178,9 +179,10 @@ module BAMIntegratedIntegratedTB ();
         enableOut  = 1'b0;
 /*---------------------------------------------------------------------------------------------------------------------------------*/
         
-        //Testcase(8): additional testcase: int x int a = 32 b = 23
+        //Testcase(8):
         // enable input registers to write a and b values
-        a = 32'd32; b = 32'd23;         
+        a = 32'hB887CAAF; 
+        b = 32'h887CAAF3;         
         enableA = 1'b1; enableB = 1'b1;
 
         #20
@@ -189,11 +191,102 @@ module BAMIntegratedIntegratedTB ();
 
         //check results
         #30
-        if(product != (32'd736)) begin
+        if(product != (64'h215D8B0A7A419A1D)) begin
             $display("TESTCASE#8 FAILED with inputs a=%d and b=%d and output product=%d", a, b, product);
         end else begin
             $display("TESTCASE#8: SUCCESS");
         end 
+        
+        #10
+        enableOut  = 1'b0;
+/*---------------------------------------------------------------------------------------------------------------------------------*/
+        
+        //Testcase(9):
+        // enable input registers to write a and b values
+        a = {32'h1};
+        b = {32'h50647236};         
+        enableA = 1'b1; enableB = 1'b1;
+
+        #20
+        // enable input registers to read a and b values -> the multiplier performs operation and outputs the product 
+        enableA = 1'b0; enableB = 1'b0; enableOut = 1'b1; 
+
+        //check results
+        #30
+        if(product != (64'h50647236)) begin
+            $display("TESTCASE#9 FAILED with inputs a=%d and b=%d and output product=%d", a, b, product);
+        end else begin
+            $display("TESTCASE#9: SUCCESS");
+        end 
+
+        #10
+        enableOut  = 1'b0;
+/*---------------------------------------------------------------------------------------------------------------------------------*/
+        
+        //Testcase(10: 
+        // enable input registers to write a and b values
+        a = {32'hB887CAAF};
+        b = {32'h1};         
+        enableA = 1'b1; enableB = 1'b1;
+
+        #20
+        // enable input registers to read a and b values -> the multiplier performs operation and outputs the product 
+        enableA = 1'b0; enableB = 1'b0; enableOut = 1'b1; 
+
+        //check results
+        #30
+        if(product != ({64'hFFFFFFFFB887CAAF})) begin
+            $display("TESTCASE#10 FAILED with inputs a=%d and b=%d and output product=%d", a, b, product);
+        end else begin
+            $display("TESTCASE#10: SUCCESS");
+        end 
+
+        #10
+        enableOut  = 1'b0;
+/*---------------------------------------------------------------------------------------------------------------------------------*/
+        
+        //Testcase(11): 
+        // enable input registers to write a and b values
+        a = {32'h0};
+        b = {32'h50647236};         
+        enableA = 1'b1; enableB = 1'b1;
+
+        #20
+        // enable input registers to read a and b values -> the multiplier performs operation and outputs the product 
+        enableA = 1'b0; enableB = 1'b0; enableOut = 1'b1; 
+
+        //check results
+        #30
+        if(product != ({64'h0})) begin
+            $display("TESTCASE#11 FAILED with inputs a=%d and b=%d and output product=%d", a, b, product);
+        end else begin
+            $display("TESTCASE#11: SUCCESS");
+        end 
+
+        #10
+        enableOut  = 1'b0;
+/*---------------------------------------------------------------------------------------------------------------------------------*/
+        
+        //Testcase(12): 
+        // enable input registers to write a and b values
+        a = {32'hB887CAAF};
+        b = {32'h0};         
+        enableA = 1'b1; enableB = 1'b1;
+
+        #20
+        // enable input registers to read a and b values -> the multiplier performs operation and outputs the product 
+        enableA = 1'b0; enableB = 1'b0; enableOut = 1'b1; 
+
+        //check results
+        #30
+        if(product != ({64'h0})) begin
+            $display("TESTCASE#12 FAILED with inputs a=%d and b=%d and output product=%d", a, b, product);
+        end else begin
+            $display("TESTCASE#12: SUCCESS");
+        end 
+
+        #10
+        enableOut  = 1'b0;
     end
 
     always #3 clk=~clk;
